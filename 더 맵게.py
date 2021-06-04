0d19f495d105679e48fd530e5d1800e769dcc738
yllausac <dadanana0805@gmail.com>
2021-06-04 15:29:13 +0900

    PS

---
+++ 더 맵게.py
@@ -0,0 +1,17 @@
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        mix = heapq.heappop(scoville) + (heapq.heappop(scoville)*2)
+        heapq.heappush(scoville, mix)
+        answer += 1
+        if len(scoville) == 1 and scoville[0] < K:
+            return - 1
+    return answer
+
+
+print(solution([1, 2, 3, 9, 10, 12], 7))
