812a589f1f679a95d31aaa3f0a0c155a0b1b4ad0
yllausac <dadanana0805@gmail.com>
2021-06-11 23:50:18 +0900

    PS

--- 연습장.py
+++ 연습장.py
@@ -1,24 +1,73 @@
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.data = None
+        self.children = {}
+        self.length = {}
+
+
+def insert(word, head):
+    curNode = head
+    w_len = len(word)
+    for w in word:
+        if w in curNode.children:
+            curNode = curNode.children[w]
+        else:
+            newNode = Node(w)
+            curNode.children[w] = newNode
+            curNode = curNode.children[w]
+        if w_len in curNode.length:
+            curNode.length[w_len] += 1
+        else:
+            curNode.length[w_len] = 1
+    curNode.data = word
+
+
 def solution(words, queries):
     answer = []
-    key, value = [], []
+    head = Node('')
+    bhead = Node('')
+    cnt_dict = {}
     for word in words:
-        key.append(list(word))
-    for query in queries:
-        value.append(list(query))
+        insert(word, head)
+        insert(word[::-1], bhead)
+        if len(word) in cnt_dict:
+            cnt_dict[len(word)] += 1
+        else:
+            cnt_dict[len(word)] = 1
 
-    for item in value:
-        m = len(item)
-        count = 0
-        for target in key:
-            match = 0
-            if len(target) == m:
-                for i in range(m):
-                    if target[i] == item[i] or item[i] == '?':
-                        match += 1
-                if match == m:
-                    count += 1
-        answer.append(count)
+    for query in queries:
+        if query[0] == '?' and query[-1] == '?':  # ??????일떄
+            try:
+                answer.append(cnt_dict[len(query)])
+            except:
+                answer.append(0)
+        else:
+            org_len = len(query)
+            if query[0] == '?':
+                query = query[::-1]
+                cur = bhead
+            else:
+                cur = head
+            query = query.split("?")[0]
+            idx = 0
+            flag = 0
+            for char in query:
+                if char in cur.children:
+                    cur = cur.children[char]
+                else:
+                    flag = 1
+                    break
+            if flag:
+                answer.append(0)
+            else:
+                try:
+                    answer.append(cur.length[org_len])
+                except:
+                    answer.append(0)
+
     return answer
+
 
 
 print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
