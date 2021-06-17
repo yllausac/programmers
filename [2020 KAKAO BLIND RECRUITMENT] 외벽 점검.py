bef692c69dd0e328aa38fe0a7c5c463d280d5984
yllausac <dadanana0805@gmail.com>
2021-06-17 21:46:57 +0900

    PS

---
+++ [2020 KAKAO BLIND RECRUITMENT] 외벽 점검.py
@@ -0,0 +1,25 @@
+def solution(n, weak, dist):
+    answer = 0
+    table = [True for _ in range(n)]
+    for i in weak:
+        table[i] = False
+    print(table)
+    dist.reverse()
+    print(dist)
+    for friend in dist:
+        for start in weak:
+            while friend > 0:
+                table[start] = True
+                friend -= 1
+                start += 1
+            if False in table:
+                continue
+            else:
+                return
+        print(table)
+
+
+    return answer
+
+
+print(solution(12, [1, 3, 4, 9, 10], [3, 5, 7]))
